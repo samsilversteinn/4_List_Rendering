d0bce33965cb73b019d9bc59f7dd54a46dd69e37
samsilversteinn <sesilversteinn@gmail.com>
2024-04-30 11:33:32 -0400

    done

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const app = Vue.createApp({
       product: 'Socks',
       image: './assets/images/socks_blue.jpg',
       inStock: true,
-      details: ['50% cotton', '30% wool', '20% polyester']
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      sizes: ['Small', 'Medium', 'Large'],
+      variants: [
+        {id: 1111, color: 'green'},
+        {id: 1112, color: 'blue'}
+      ]
     }
   }
 })
