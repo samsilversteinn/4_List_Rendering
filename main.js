ca4e61e10f4af10ab1cb57672bc71daea1148921
samsilversteinn <sesilversteinn@gmail.com>
2024-05-02 12:42:39 -0400

    final

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const app = Vue.createApp({
-  data () {
+  data() {
     return {
+      cart: 0,
       product: 'Socks',
       image: './assets/images/socks_blue.jpg',
       inStock: true,
       details: ['50% cotton', '30% wool', '20% polyester'],
-      sizes: ['Small', 'Medium', 'Large'],
       variants: [
-        {id: 1111, color: 'green'},
-        {id: 1112, color: 'blue'}
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
       ]
+    }
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    removeFromCart() {
+      this.cart > 0 && this.cart --
+    },
+    updateImage(variantImage) {
+      this.image = variantImage
     }
   }
 })
