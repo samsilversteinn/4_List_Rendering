d6672d5740ff17007dcea84217fc1480a6832a61
samsilversteinn <123968150+samsilversteinn@users.noreply.github.com>
2024-05-02 12:50:31 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,15 @@
 const app = Vue.createApp({
-  data() {
+  data () {
     return {
-      cart: 0,
       product: 'Socks',
       image: './assets/images/socks_blue.jpg',
       inStock: true,
       details: ['50% cotton', '30% wool', '20% polyester'],
+      sizes: ['Small', 'Medium', 'Large'],
       variants: [
-        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+        {id: 1111, color: 'green'},
+        {id: 1112, color: 'blue'}
       ]
-    }
-  },
-  methods: {
-    addToCart() {
-      this.cart += 1
-    },
-    removeFromCart() {
-      this.cart > 0 && this.cart --
-    },
-    updateImage(variantImage) {
-      this.image = variantImage
     }
   }
 })
